7489608db15a7fa9081a22a53c4b4eada9f86eb1
Martin Beneš <martinbenes626@gmail.com>
2025-05-25 20:33:40 +0200

    more fish configs

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,22 @@ if status is-interactive
     set -gx XDG_CONFIG_HOME ~/.config
     set -gx PAGER less
     starship init fish | source
-    zoxide init fish | source
-    thefuck --alias | source
+    # zoxide init fish | source
+end
+# source /etc/profile with bash
+if status is-login
+    if not set -q __sourced_profile
+        set -x __sourced_profile 1
+        exec zsh -c "\
+            test -e /etc/profile && source /etc/profile
+            test -e $HOME/.zprofile && source $HOME/.zprofile
+            exec fish --login
+        "
+    end
+    
+    # put your other configs below
+    
+    #
+    
+    set -e __sourced_profile
 end
