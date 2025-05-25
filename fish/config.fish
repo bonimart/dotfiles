6715bdb99986e05ec35fb53b8e5ac676caf31a52
Martin Beneš <martinbenes626@gmail.com>
2025-05-25 20:48:04 +0200

    abbreviations

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,7 @@ if status is-interactive
     set -gx XDG_CONFIG_HOME ~/.config
     set -gx PAGER less
     starship init fish | source
-    # zoxide init fish | source
+    zoxide init fish | source
 end
 # source /etc/profile with bash
 if status is-login
@@ -23,3 +23,17 @@ if status is-login
     
     set -e __sourced_profile
 end
+# git aliases
+abbr gst 'git status'
+abbr ga 'git add'
+abbr gc 'git commit'
+abbr gb 'git branch'
+abbr gco 'git checkout'
+abbr gcb 'git checkout -b'
+abbr gd 'git diff'
+abbr gl 'git log'
+abbr glo 'git log --oneline --graph --decorate'
+abbr gp 'git push'
+abbr gl 'git pull'
+alias grep rg
+alias cd z
